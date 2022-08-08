fefbd22d59e956899b78635f1b9a7a4b1e1756c2
RAHULMANDAL100 <91194192+RAHULMANDAL100@users.noreply.github.com>
2022-08-08 12:20:33 +0530

    Add files via upload

---
+++ Tribute Page-Task2/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+  }
+
+  var audio = new Audio("Sachin sachin.mp3");
+
+ function musicplay(){
+	audio.play();
+ }
